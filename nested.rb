10fdb6bad2cd09bb4575bde476f9d277b392ff34
simon cohen <escohen115@gmail.com>
2020-10-15 00:20:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-		lang = programmer_hash[:dennis_ritchie][:languges][0]
+		lang = programmer_hash[:dennis_ritchie][:languges].first
 lang
 end
 
